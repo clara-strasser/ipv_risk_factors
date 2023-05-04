6a3a449849fd4b6bdd089efe7804c1ccb02029e0
Clara Strasser <clara@Claras-Air.fritz.box>
2023-05-04 11:02:49 +0200

    structuring of folders

---
+++ read_data.R
@@ -0,0 +1,17 @@
+# Read Data and Save ---------
+
+## Set path ----
+path <- "/Users/clara/Desktop/Masterarbeit/endireh_documents/2021/data"
+setwd(path)
+
+## Load data ----
+endireh2021 <- load("bd_endireh_2021.RData")
+
+## Save separated ----
+save(TB_SEC_III, file = "TB_SEC_III.RData")
+save(TB_SEC_IVaVD, file = "TB_SEC_IVaVD.RData")
+save(TSDem, file = "TSDem.RData")
+save(TVIV, file = "TVIV.RData")
+
+
+
