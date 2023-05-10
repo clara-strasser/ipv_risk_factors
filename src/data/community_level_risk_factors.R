b409b2d461e20d5ea2c2c06113d8697ca9223988
Clara Strasser <clara@Claras-Air.fritz.box>
2023-05-10 18:04:07 +0200

    undp source

--- src/data/community_level_risk_factors.R
+++ src/data/community_level_risk_factors.R
@@ -11,6 +11,14 @@ library(readxl)
 path <- "/Users/clara/Desktop/Masterarbeit/endireh_documents/2021/data/"
 
 ## Load data -------
+
+
+# Risk Factors -----
+
+## GINI INDEX ----
+# Variable name: gini20
+# Outcome: 0-1
+# Level: municipality
 coneval <- read_excel(paste0(path,"coneval_2023.xlsx"), sheet = 3, skip = 2, range = "A8:F2479", col_names = TRUE)
 coneval <- coneval[-c(1:2), -1]
 coneval <- coneval %>%
@@ -19,16 +27,13 @@ coneval <- coneval %>%
          gini20 = "Coeficiente de Gini") %>%
   select(-c("Entidad  federativa", "Municipio"))
 
-# Risk Factors -----
-
-## GINI INDEX ----
-# Variable name: gini20
+## Human Developement Index -----
+# Variable name: idh2020
 # Outcome: 0-1
 # Level: municipality
-
-
-
-
+undp <- read_excel(paste0(path,"undp_2020.xlsx"), sheet = 1, col_names = TRUE)
+undp <- undp %>%
+  select(c("AGEE", "AGEM", "IDH"))
 
 
 
