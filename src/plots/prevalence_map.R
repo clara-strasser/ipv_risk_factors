428ccc830d4204d2ed49acc73da6b6614436eeda
Clara Strasser <clara@Claras-MacBook-Air.local>
2023-08-17 18:12:45 +0200

    finished prevalence plot

--- src/plots/prevalence_map.R
+++ src/plots/prevalence_map.R
@@ -63,32 +63,83 @@ e_vpar_lr_con <- svyby(~vpar_12m_con, denominator=~par_12m, by=~NOM_ENT, disenio
 # Plots ---------
 
 ## Line Plot ------
+
+# Prep
 prevalence_state <- as.data.frame(e_vpar_lr_con)
 colnames(prevalence_state)[2] <- "prevalence"
 colnames(prevalence_state)[3] <- "se"
 prevalence_state$NOM_ENT <- str_to_title(tolower(prevalence_state$NOM_ENT))
 prevalence_state$NOM_ENT <- factor(prevalence_state$NOM_ENT, levels = prevalence_state$NOM_ENT[order(prevalence_state$prevalence, decreasing = TRUE)])
 
-# Plot 
+# Add confidence interval
+z <- qnorm(0.95) # Z-score for 90% confidence interval
+prevalence_state$Lower_CI <- prevalence_state$prevalence - z * prevalence_state$se
+prevalence_state$Upper_CI <- prevalence_state$prevalence + z * prevalence_state$se
+
+
+# Plot
 prevalence_line_plot <- ggplot(prevalence_state, aes(x = NOM_ENT, y = prevalence)) +
   geom_vline(aes(xintercept = NOM_ENT), linetype = "dashed", color = "gray") +
   geom_point(color = "#620042") +
-  geom_errorbar(aes(ymin = prevalence - se, ymax = prevalence + se), width = 0.2, color = "#620042") +
-  labs(x = "State", y = "State Level Prevalence of Emotional IPV") +
+  geom_errorbar(aes(ymin = Lower_CI, ymax = Upper_CI), width = 0.2, color = "#620042") +
+  labs(x = "Federal State", y = "State Level Prevalence of Emotional IPV") +
   theme_minimal() +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
-        axis.line = element_line(color = "black"),
-        axis.title = element_text(size = 14),
+  theme(axis.line = element_line(color = "black"),
+        axis.title = element_text(size = 16),
         axis.ticks.y = element_line(),
-        axis.ticks.x = element_line(),
-        plot.title = element_text(face = "bold", hjust = 0.5, size = 16, color = "black"),
+        axis.text.x = element_text(size = 12, angle = 90, vjust = 0.5, hjust = 1),
+        axis.text.y = element_text(size = 14),
+        plot.title = element_text(face = "bold", hjust = 0.5, size = 16),
         legend.title = element_text(size = 20, face = "bold"),
         legend.text = element_text(size = 13),
         legend.key.size = unit(15, "points"),
         legend.position = c(0.95, 0.95),  # Adjust the position as needed
         legend.justification = c(1, 1),
-        legend.box.just = "right")
-prevalence_line_plot
+        legend.box.just = "right") +
+  scale_y_continuous(breaks = seq(0, 0.5, by = 0.1), limits = c(0, 0.5)) 
+ggsave(plot = prevalence_line_plot, filename = "prevalence_line_plot.png", path = save_path, width = 15, height = 8) # Save the plot file by copying it
+
+
+## Map plot -----
+
+# Get prevalence state again (for CVE_ENT)
+e_vpar_lr_con <- svyby(~vpar_12m_con, denominator=~par_12m, by=~CVE_ENT, disenio,  svyratio, na.rm = TRUE)  
+
+# Modify
+prevalence_state <- as.data.frame(e_vpar_lr_con)  
+colnames(prevalence_state)[2] <- "prevalence"
+colnames(prevalence_state)[3] <- "se"
+
+# Left join
+prevalence <- shapefile_df %>%
+  left_join(prevalence_state, by = c("CVE_ENT"))
+
+# Plot
+prevalence_map_plot <- ggplot() +
+  geom_sf(data = prevalence, aes(fill = prevalence), color = "black") +
+  geom_sf_label(data = prevalence, aes(label = CVE_ENT),
+                fill = "violet", size = 2, nudge_y = 0.02, parse = TRUE) +
+  scale_fill_gradient2("Emotional IPV Prevalence",high = "#1a0946", 
+                       limits= c(0,0.5), 
+                       breaks = c(0, 0.1, 0.2,0.3,0.4,0.5),
+                       labels = c("0", "0.1", "0.2","0.3", "0.4", "0.5")) +
+  theme_void()+
+  theme(legend.title = element_text(size = 20, face = "bold"),
+        legend.text = element_text(size = 13),
+        legend.key.size = unit(15, "points"),
+        legend.position = c(0.95, 0.95),  # Adjust the position as needed
+        legend.justification = c(1, 1),
+        legend.box.just = "right",
+        axis.text = element_text(face = "bold"),
+        axis.text.x = element_blank(),  # Remove x-axis labels
+        axis.text.y = element_blank(),  # Remove y-axis labels
+        axis.title.x = element_blank(), # Remove x-axis title
+        axis.title.y = element_blank())
+
+
+# Save the plots -----
+ggsave(plot = prevalence_map_plot, filename = "prevalence_map_plot.png", path = save_path, width = 15, height = 8) # Save the plot file by copying it
+
 
 
 
