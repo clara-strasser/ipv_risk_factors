cc98e15ec284087ca9b9d01aff6f4ccc3b95f634
Clara Strasser <clara@q045.lmu.vpn.lrz.de>
2023-09-20 10:50:10 +0200

    changes

--- src/rob/gam_analysis.R
+++ src/rob/gam_analysis.R
@@ -7,6 +7,7 @@
 library(dplyr)
 library(tidyr)
 library(mgcv)
+library(margins)
 
 ## Set path -----
 path_data <- "/Users/clara/Desktop/master_thesis/r_projects/ipv_risk_factors/data/final_data/"
@@ -43,7 +44,7 @@ data$vio_emo_año <- factor(data$vio_emo_año, levels = c("no", "yes"), labels =
 ### GAM with linear effects ------
 offset_var <- rep(pnorm(weighted.mean(x = as.numeric(as.character(data[, "vio_emo_año"])),
                                       w = data$FAC_MUJ))-0.5, nrow(data))
-model <- gam(vio_emo_año ~ desempleo + vio_inf + vio_exp_inf + 
+model <- gam(vio_emo_año ~ eda_sex + desempleo + vio_inf + vio_exp_inf + 
                vio_sex_inf + con_sex + mot_mat + vio_inf_par + 
                vio_exp_inf_par + act_distboth + act_distmales + lib_sex_gradmedium +
              lib_eco_gradmedium + redsoc_gradhigh + rout_gradmedium + MasNoDen, 
@@ -53,6 +54,8 @@ model <- gam(vio_emo_año ~ desempleo + vio_inf + vio_exp_inf +
              data = data)
 summary(model)
 
+### Plot effects
+plot(model, all.terms = TRUE)
 
 
 
@@ -60,6 +63,46 @@ summary(model)
 
 
 
+plot(x = data$EDAD, 
+     y = data$vio_emo_año,
+     main = "Probit Model of the Probability of Denial, Given P/I Ratio",
+     xlab = "Eda Sex",
+     ylab = "VIO",
+     pch = 20,
+     ylim = c(0, 0.1),
+     cex.main = 0.85)
+
+# add horizontal dashed lines and text
+abline(h = 1, lty = 2, col = "darkred")
+abline(h = 0, lty = 2, col = "darkred")
+text(2.5, 0.9, cex = 0.8, "Mortgage denied")
+text(2.5, -0.1, cex= 0.8, "Mortgage approved")
+
+# add estimated regression line
+x <- seq(0, 3, 0.01)
+y <- predict(model, list(EDAD = x), type = "response")
+
+lines(x, y, lwd = 1.5, col = "steelblue")
+
+data$EDAD <- data$EDAD + 40.59427
+
+
+
+
+model <- glm(vio_emo_año ~ EDAD + eda_sex:con_sex + mot_mat:eda_mat + 
+               lib_eco_gradmedium + redsoc_gradmedium + act_distmales +
+               pea_f + MasPrev,
+             family = binomial(link = "probit"),
+             data = data)
+
+
+, 
+             weights = data$FAC_MUJ,
+             offset = offset_var_authors,
+             data = data)
+plot(model, all.terms = TRUE)
+marg <-margins(model)
+model
 
 
 
