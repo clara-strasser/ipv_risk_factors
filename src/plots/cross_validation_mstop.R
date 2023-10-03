01a20764642a89351cd1fa6208e36412852f02ae
Clara Strasser <clara@s037.lmu.vpn.lrz.de>
2023-10-03 13:42:14 +0200

    newest changes

--- src/plots/cross_validation_mstop.R
+++ src/plots/cross_validation_mstop.R
@@ -9,24 +9,29 @@ library(ggplot2)
 library(mboost)
 
 ## Set path -----
-path_data <- ""
+path_data <- "/Users/clara/Desktop/master_thesis/results/model1_imputed/"
 
 ## Load data -------
-load("~/cvemoipv.RData") # cross-validation results
+load(paste0(path_data,"cv1.RData")) # cross-validation results
 
 # Plot -----
+setwd("/Users/clara/Desktop/master_thesis/r_projects/ipv_risk_factors/results/plots")
 
 # Number of boosting iterations
 cross_validation_model1 <- plot(cvemoipv,
      ylim = c(0.45, 0.55),
      xlab = "Number of Boosting Iterations",
      ylab = "Average Empirical Risk",
-     main = "Cross-Validation Method: Subsampling")
+     main = "Subsampling")
 
 # Save ----
+png("cross_validation_model1.png", width = 800, height = 600)
+print(cross_validation_model1)
+dev.off()
+
 
 # Save in folder
-ggsave("~/ipv_risk_factors/results/plots/cross_validation_model1.png")
+ggsave("/Users/clara/Desktop/master_thesis/r_projects/ipv_risk_factors/results/plots/cross_validation_model1.png")
 
 
 
